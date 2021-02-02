7bd82023b3c702ea2fe06553455c0af0ef080730
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-02-02 12:38:09 +0200

    now it's GOOD (untested)

--- src/main/kotlin/io/github/ytg1234/vina/impl/api/Wheighable.kt
+++ src/main/kotlin/io/github/ytg1234/vina/impl/api/Wheighable.kt
@@ -12,13 +12,23 @@ import net.minecraft.item.ItemStack
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.util.math.MathHelper
 import org.jetbrains.annotations.Range
+import kotlin.math.min
 
 class EntityWeighable(private val ett: Entity) : Weighable {
     override val weight: @Range(from = 0, to = 15) Int
         get() {
             var initialWeight = WeightManager[ett.type]
-            if (ett is ItemEntity && ett.stack.item is BlockItem && ett.stack.orCreateTag.contains("Items") && config.entityWeightConfig.isInventoryAddedToItemWeight) {
-                initialWeight += fromTagButGood(ett.stack.orCreateTag).size
+            if (ett is ItemEntity && ett.stack.item is BlockItem && (ett.stack.orCreateTag.contains("BlockEntityTag") || ett.stack.orCreateTag.contains(
+                    "Items"
+                )) && config.entityWeightConfig.isInventoryAddedToItemWeight
+            ) {
+                initialWeight += calculateRedstoneSignal(
+                    fromTagButGood(
+                        if (ett.stack.orCreateTag.contains("BlockEntityTag")) ett.stack.orCreateTag.getCompound(
+                            "BlockEntityTag"
+                        ) else ett.stack.orCreateTag
+                    )
+                )
             } else if (ett is LivingEntity && ett.hasStatusEffect(StatusEffects.SLOWNESS) && config.entityWeightConfig.isSlownessAddedToEntityWeight) {
                 initialWeight += ett.getStatusEffect(StatusEffects.SLOWNESS)!!.amplifier + 1
             }
@@ -26,7 +36,7 @@ class EntityWeighable(private val ett: Entity) : Weighable {
         }
 }
 
-fun fromTagButGood(tag: CompoundTag): List<ItemStack> {
+internal fun fromTagButGood(tag: CompoundTag): List<ItemStack> {
     val ls = mutableListOf<ItemStack>()
     val listTag = tag.getList("Items", 10)
 
@@ -35,4 +45,12 @@ fun fromTagButGood(tag: CompoundTag): List<ItemStack> {
     }
 
     return ls
+}
+
+internal fun calculateRedstoneSignal(inventory: List<ItemStack>): Int {
+    val filledStacks = inventory.filter { !it.isEmpty }
+    val total = filledStacks.fold(0.0) { acc, stack ->
+        acc + stack.count.toDouble() / min(64, stack.maxCount).toDouble()
+    }
+    return MathHelper.floor((total / inventory.size.toDouble()) * 13.0) + if (filledStacks.isNotEmpty()) 1 else 0
 }
